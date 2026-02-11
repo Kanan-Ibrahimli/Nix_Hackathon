0e8e21967bafbb80f3f9b6a3d4508eedb138358e
Kanan Ibrahimli <kanan.ibrahimli@nixs.com>
2026-02-11 15:16:54 +0100

    add typescript

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Health Check Endpoint
+app.get('/healthz', (req: Request, res: Response) => {
+  res.status(200).json({ status: 'OK' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
